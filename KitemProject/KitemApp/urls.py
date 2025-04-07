c1b2fd095d35d4b777fc146dc2421ca7e7011ebd
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2025-04-07 20:04:48 -0300

    adiciona funcionalidades para gerenciar ingredientes: criar, atualizar e deletar, além de atualizar templates e URLs correspondentes

--- KitemProject/KitemApp/urls.py
+++ KitemProject/KitemApp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('ingredientes/', views.ingredientes, name='ingredientes'),
-    # path('todos/', views.todos, name='todos'),
+    path('novo_ingrediente/', views.novo_ingrediente, name='novo_ingrediente'),
+    path('ingredientes/atualiza/<int:id_ingrediente>/', views.atualiza_ingrediente, name='atualiza_ingrediente'),
+    path('ingredientes/deleta/<int:id_ingrediente>/', views.deleta_ingrediente, name='deleta_ingrediente'),
 ]
