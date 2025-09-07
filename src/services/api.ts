3d76adac99bc0a907c983268edf30ea130ed05c9
Marcos Prado <marcospaulo_prado@live.com>
2025-09-07 18:17:16 -0300

    api service alterado para funcionar tanto em produção quanto em desenvolvimento

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,11 +2,9 @@ import axios from "axios";
 
 const getBaseURL = () => {
   if (import.meta.env.PROD) {
-    // Em produção, aponte diretamente para o seu backend
     return 'https://back-kitem-e12u.onrender.com';
   }
-  // Em desenvolvimento, use uma URL relativa para que o proxy do Vite funcione
-  return '';
+  return '/api';
 };
 
 const api = axios.create({
